cb07409cefaf52246027f0b843c3ea76a48063b0
Crew <45433487+Crufro@users.noreply.github.com>
2026-05-19 20:24:15 -0400

    updated design

--- boxes/generators/dualdellmicro1urackmount.py
+++ boxes/generators/dualdellmicro1urackmount.py
@@ -13,6 +13,8 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import copy
+
 from boxes import *
 
 
@@ -25,56 +27,88 @@ class DualDellMicro1URackMount(Boxes):
         Boxes.__init__(self)
         self.addSettingsArgs(edges.FingerJointSettings, surroundingspaces=0.5)
         self.argparser.add_argument(
-            "--depth", action="store", type=float, default=175.,
+            "--depth", action="store", type=float, default=200.,
             help="inner depth in mm")
+        self.argparser.add_argument(
+            "--front_thickness", action="store", type=float, default=0.0,
+            help="thickness of the front panel in mm (defaults to material thickness)")
         self.argparser.add_argument(
             "--height", action="store", type=int, default=1,
             choices=list(range(1, 17)),
             help="height in rack units")
         self.argparser.add_argument(
             "--triangle", action="store", type=float, default=25.,
-            help="Sides of the triangles holding the lid in mm")
-        self.argparser.add_argument(
-            "--d1", action="store", type=float, default=2.,
-            help="Diameter of the inner lid screw holes in mm")
-        self.argparser.add_argument(
-            "--d2", action="store", type=float, default=3.,
-            help="Diameter of the lid screw holes in mm")
+            help="Sides of the corner support triangles in mm")
 
-    def wallxCB(self):
+    def _setup_front_finger_joints(self):
+        front_t = self.front_thickness or self.thickness
+        self.front_t = front_t
+        front_settings = copy.deepcopy(self.edges["f"].settings)
+        front_settings.thickness = front_t
+        front_settings.edgeObjects(self, chars="gGH")
+        self.frontFingerHolesAt = edges.FingerHoles(self, front_settings)
+
+    def wallxCB(self, fingerHoles=None):
         t = self.thickness
+        fingerHoles = fingerHoles or self.fingerHolesAt
         self.fingerHolesAt(0, self.h-1.5*t, self.triangle, 0)
         self.fingerHolesAt(self.x, self.h-1.5*t, self.triangle, 180)
-        self.fingerHolesAt(self.x / 2., 0, self.h, 90)
+        self.centerDividerFingerHoles(fingerHoles)
+
+    def wallxFrontCB(self):
+        ht = self.frontFingerHolesAt.settings.thickness
+        edge_width = self.frontFingerHolesAt.settings.edge_width
+        corner_y = self.h - edge_width - 0.5 * ht + max(0.0, ht - self.thickness)
+        self.frontFingerHolesAt(0, corner_y, self.triangle, 0)
+        self.frontFingerHolesAt(self.x, corner_y, self.triangle, 180)
+        self.centerDividerFingerHoles(self.frontFingerHolesAt)
+
+    def centerDividerFingerHoles(self, fingerHoles):
+        settings = fingerHoles.settings
+        hole_spacing = settings.finger + settings.space
+        start = 0.5 * (self.h - hole_spacing)
+        with self.saved_context():
+            self.moveTo(self.x / 2., start, 90)
+            for pos in (0.0, hole_spacing):
+                self.rectangularHole(
+                    pos + 0.5 * settings.finger,
+                    0,
+                    settings.finger + settings.play,
+                    settings.width + settings.play)
 
     def bottomCB(self):
         self.fingerHolesAt(self.x / 2., 0, self.y, 90)
 
     def wallxbCB(self): # back
         self.wallxCB()
-        power_r = 5.0      # ~10 mm hole, snug for a power barrel/cable
-        ethernet_r = 7.0   # ~14 mm hole, fits an RJ45 boot
+        cable_hex_r = 9.0  # about 15.6 mm across flats for power barrels and RJ45 boots
+        cable_corner_r = 1.5
         cy = self.h / 2.
         eighth = self.x / 8.
         # Both Optiplexes oriented identically; each has power on its left
         # (viewed from behind) and ethernet on its right. Pattern across
         # the back, outer-wall to outer-wall: P - E - | - P - E
-        self.hole(1. * eighth, cy, r=ethernet_r)
-        self.hole(3. * eighth, cy, r=power_r)
-        self.hole(5. * eighth, cy, r=ethernet_r)
-        self.hole(7. * eighth, cy, r=power_r)
+        self.regularPolygonHole(
+            1. * eighth, cy, r=cable_hex_r, n=6, corner_radius=cable_corner_r)
+        self.regularPolygonHole(
+            3. * eighth, cy, r=cable_hex_r, n=6, corner_radius=cable_corner_r)
+        self.regularPolygonHole(
+            5. * eighth, cy, r=cable_hex_r, n=6, corner_radius=cable_corner_r)
+        self.regularPolygonHole(
+            7. * eighth, cy, r=cable_hex_r, n=6, corner_radius=cable_corner_r)
 
     def wallxfCB(self): # front
         t = self.thickness
-        for x in (8.5, self.x+2*17.+2*t-8.5):
+        front_t = self.front_t
+        for x in (8.5, self.x + 2 * 17. + 2 * front_t - 8.5):
             for y in (6., self.h-6.+t):
                 self.rectangularHole(x, y, 10, 6.5, r=3.25)
 
-        slot_w, slot_h, edge_margin = 180., 31., 10.
+        slot_w, slot_h, edge_margin = 184., 31., 10.
         tab_w, tab_h, fillet_r = 18., 7., 3.
-        cy = (self.h + t) / 2.
-        left_cx = t + 17. + edge_margin + slot_w / 2.
-        right_cx = t + 17. + self.x - edge_margin - slot_w / 2.
+        cy = (self.h + t) / 2. + max(0.0, self.front_t - t) + 2.0
+        left_cx = front_t + 17. + edge_margin + slot_w / 2.
+        right_cx = front_t + 17. + self.x - edge_margin - slot_w / 2.
         # left slot: L-shape with notch at top-left, fillet at inner corner
         with self.saved_context():
             BL_x = left_cx - slot_w / 2.
@@ -117,16 +151,61 @@ class DualDellMicro1URackMount(Boxes):
             )
 
         self.moveTo(t+17., t)
-        self.wallxCB()
+        self.wallxFrontCB()
 
     def wallyCB(self):
         t = self.thickness
         self.fingerHolesAt(0, self.h-1.5*t, self.triangle, 0)
         self.fingerHolesAt(self.y, self.h-1.5*t, self.triangle, 180)
 
+    def _frontFlangedWall(self, x, y, edges="FFFF", flanges=None, r=0.0,
+                          callback=None, move=None, label=""):
+        t = self.frontFingerHolesAt.settings.thickness
+
+        if not flanges:
+            flanges = [0.0] * 4
+
+        while len(flanges) < 4:
+            flanges.append(0.0)
+
+        edges = [self.edges.get(e, e) for e in edges]
+        edges = edges + edges
+        flanges = flanges + flanges
+
+        tw = x + edges[1].spacing() + flanges[1] + edges[3].spacing() + flanges[3]
+        th = y + edges[0].spacing() + flanges[0] + edges[2].spacing() + flanges[2]
+
+        if self.move(tw, th, move, True):
+            return
+
+        rl = min(r, max(flanges[-1], flanges[0]))
+        self.moveTo(rl + edges[-1].margin(), edges[0].margin())
+
+        for i in range(4):
+            l = y if i % 2 else x
+
+            rl = min(r, max(flanges[i-1], flanges[i]))
+            rr = min(r, max(flanges[i], flanges[i+1]))
+            self.cc(callback, i, x=-rl)
+            if flanges[i]:
+                if edges[i] in (self.edges["G"], self.edges["H"]):
+                    self.frontFingerHolesAt(
+                        flanges[i-1] + edges[i-1].endWidth() - rl,
+                        0.5*t + flanges[i], l, angle=0)
+                self.edge(l + flanges[i-1] + flanges[i+1] +
+                          edges[i-1].endWidth() + edges[i+1].startWidth() -
+                          rl - rr)
+            else:
+                self.edge(flanges[i-1] + edges[i-1].endWidth() - rl)
+                edges[i](l)
+                self.edge(flanges[i+1] + edges[i+1].startWidth() - rr)
+            self.corner(90, rr)
+        self.move(tw, th, move, label=label)
+
 
     def _render(self, type):
 
+        self._setup_front_finger_joints()
         t = self.thickness
         self.h = h = self.height * 44.45 - 0.787 - t
         if type == 10:
@@ -136,27 +215,25 @@ class DualDellMicro1URackMount(Boxes):
         x = self.x
         y = self.y = self.depth
 
-        d1, d2 =self.d1, self.d2
         tr = self.triangle
-        trh = tr / 3.
 
-        self.rectangularWall(y, h, "ffef", callback=[self.wallyCB],
+        self.rectangularWall(y, h, "ffeg", callback=[self.wallyCB],
                              move="right", label="right")
-        self.flangedWall(x, h, "FFEF", callback=[self.wallxfCB], r=t,
-                         flanges=[0., 17., -t, 17.], move="up", label="front")
+        self._frontFlangedWall(x, h, "GGEG", callback=[self.wallxfCB], r=t,
+                               flanges=[0., 17., -t, 17.], move="up", label="front")
         self.rectangularWall(x, h, "fFeF", callback=[self.wallxbCB],
                              label="back")
-        self.rectangularWall(y, h, "ffef", callback=[self.wallyCB],
+        self.rectangularWall(y, h, "ffeg", callback=[self.wallyCB],
                              move="left up", label="left")
 
-        self.rectangularWall(x, y, "fFFF", callback=[self.bottomCB],
+        self.rectangularWall(x, y, "gFFF", callback=[self.bottomCB],
                              move="up", label="bottom")
-        self.rectangularWall(y, h, "ffef", move="up", label="middle")
-        self.rectangularWall(x, y, callback=[
-            lambda:self.hole(trh, trh, d=d2)] * 4, move='right', label="lid")
+        self.rectangularWall(y, h, "ffeg", move="up", label="middle")
 
-        self.rectangularTriangle(tr, tr, "ffe", num=4,
-            callback=[None, lambda: self.hole(trh, trh, d=d1)])
+        support_r = tr
+        self.rectangularTriangle(tr, tr, "gfe", r=support_r, num=2, move="right",
+            callback=[None, None])
+        self.rectangularTriangle(tr, tr, "ffe", r=support_r, num=2)
 
 
     def render(self):
